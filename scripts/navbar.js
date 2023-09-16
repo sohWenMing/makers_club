094dde0731c0e4deee5234580c19be28a0ad9814
Soh Wen Ming <wenming.soh@gmail.com>
2023-09-17 05:27:03 +0800

    managed mobile navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('loaded')
-    const hamburgerButton = document.getElementById('hamburger-icon')
-    const mobileNav = document.querySelector('.mobile-navbar')
-    hamburgerButton.addEventListener("click", () => {
-        console.log("button loaded")
-        mobileNav.classList.toggle("open");
-    })
-})
+  console.log("loaded");
+  const hamburgerButton = document.getElementById("hamburger-icon");
+  const hamburgerLines = hamburgerButton.querySelectorAll("div");
+  const mobileNav = document.querySelector(".mobile-navbar");
+  const openToggles = [...hamburgerLines, mobileNav];
+  console.log(openToggles);
+
+  hamburgerButton.addEventListener("click", () => {
+    console.log("button loaded");
+    openToggles.forEach((element) => {
+      element.classList.toggle("open");
+    });
+  });
+});
