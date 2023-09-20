e793e57c81b24c0b5061adf2fc68f3e1a5a5bcbc
Soh Wen Ming <wenming.soh@gmail.com>
2023-09-21 06:19:56 +0800

    fixed index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,5 @@ const homeRoutes = require("./homeRoutes");
 const testRoutes = require("./testRoutes");
 
 module.exports = function (app) {
-  // app.use("/test", (req, res) => {
-  //     res.send("getting something from test.")
-  // });
   app.use("/", testRoutes, homeRoutes);
-  //   app.use("/", homeRoutes);
 };
