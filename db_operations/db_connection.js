8f46dbc5552932467f715dceea0ec80102138d43
Soh Wen Ming <wenming.soh@gmail.com>
2023-10-15 00:58:38 +0800

    refactored login

--- db_operations/db_connection.js
+++ db_operations/db_connection.js
@@ -5,8 +5,9 @@ const util = require("util");
 const DBSOURCE = "./database/makers.db";
 const db = new sql.Database(DBSOURCE);
 const dbAll = util.promisify(db.all.bind(db));
+const dbGet = util.promisify(db.get.bind(db));
 
-module.exports = { db, dbAll };
+module.exports = { db, dbAll, dbGet };
 
 //  to be refactored later. DBSOURCE here is
 //  ./database/makers.db because of where db_connection is called from. as this is called from loginRoutes.js,
